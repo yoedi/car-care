b09fdb8ce8a7dff29f344ae5929d64aeeb7e686f
Yoedi Arianto <yoediarianto@gmail.com>
2024-11-22 09:42:16 +0700

    integrasi halaman home

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CarService;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    //
+    public function index() {
+        $cities = City::all();
+        $services = CarService::withCount(['storeServices'])->get();
+
+        return view('front.index', compact('cities', 'services'));
+    }
 }
