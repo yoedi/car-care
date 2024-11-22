b48c1ef8d86eef80b5cc3a13dddea74e222224a9
Yoedi Arianto <yoediarianto@gmail.com>
2024-11-22 16:52:59 +0700

    Add feature store search

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\CarService;
+use App\Models\CarStore;
 use App\Models\City;
 use Illuminate\Http\Request;
 
@@ -13,5 +14,29 @@ class FrontController extends Controller
         $services = CarService::withCount(['storeServices'])->get();
 
         return view('front.index', compact('cities', 'services'));
+    }
+
+    public function search(Request $request) {
+        $cityId = $request->input('city_id');
+        $serviceTypeId = $request->input('service_type');
+
+        $carService = CarService::where('id', $serviceTypeId)->first();
+        if (!$carService) {
+            return redirect()->back()->with('error', 'Service type not found');
+        }
+
+        $stores = CarStore::whereHas('storeServices', function($query) use ($carService) {
+            $query->where('car_service_id', $carService->id);
+        })->where('city_id', $cityId)->get();
+
+        $city = City::find($cityId);
+
+        session()->put('serviceTypeId', $request->input('service_type'));
+
+        return view('front.stores', [
+            'stores' => $stores,
+            'carService' => $carService,
+            'cityName' => $city ? $city->name : 'Unknown City',
+        ]);
     }
 }
